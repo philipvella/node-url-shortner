b22d47a810359ef909cf82e5870843ae3ee9c5cc
Philip Vella <4180985+philipvella@users.noreply.github.com>
2023-09-01 12:50:02 +0100

    avoiding undefined in path

--- app/routes/l.$linkId.tsx
+++ app/routes/l.$linkId.tsx
@@ -2,8 +2,9 @@ import fs from "fs";
 import {LoaderArgs, redirect} from "@remix-run/node";
 
 import 'dotenv/config'
-const publicPath = `${process.env.PUBLIC_PATH}urls.json`
-export const loader = ({params}:LoaderArgs) => {
+
+const publicPath = `${process.env.PUBLIC_PATH || ''}urls.json`
+export const loader = ({params}: LoaderArgs) => {
     let urlMappings: { [index: string]: string } = {};
     try {
         const data = fs.readFileSync(publicPath, 'utf8');
@@ -13,10 +14,10 @@ export const loader = ({params}:LoaderArgs) => {
     }
 
     const originalUrl = urlMappings[`${params.linkId}`];
-    if(originalUrl)
-    return redirect(originalUrl);
+    if (originalUrl)
+        return redirect(originalUrl);
 }
 
 export default function PostRoute() {
-  return (<>Url Not found</>)
+    return (<>Url Not found</>)
 }
